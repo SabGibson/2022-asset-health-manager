8df4307568d079ebbc99bed2c4ab7cb22f7a8eae
SabGibson <sabelogibson1@gmail.com>
2022-09-13 22:43:21 +0100

    program framework complete of main() helper functions

--- run.py
+++ run.py
@@ -1,3 +1,37 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Import external libraries for working with google drive adn sheets API.
+...
+"""
+from pprint import pprint
+import sys
+import gspread
+from google.oauth2.service_account import Credentials
+from time import sleep
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open("finance-health-manager-db")
+
+users = SHEET.worksheet("users")
+salary = SHEET.worksheet("salary")
+balance_sheet = SHEET.worksheet("balance-sheet")
+insights = SHEET.worksheet("insights")
+
+
+def main ():
+    splansh_screen()
+    user_choice = welcome_screen()
+
+    if (user_choice == "q"):
+        print("Thank you! Bye!")
+        sleep(3)
+        sys.exit()
+    else:
+        launch_interface(user_choice)
